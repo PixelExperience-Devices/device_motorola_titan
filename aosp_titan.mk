6e569f58512caa35fe992dd1ddc0dedc60e47a10
RenanQueiroz <queirozrrq@gmail.com>
2018-05-28 04:48:26 -0400

    Fix deps

--- aosp_titan.mk
+++ aosp_titan.mk
@@ -1,12 +1,10 @@
 $(call inherit-product, device/motorola/titan/full_titan.mk)
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
 TARGET_GAPPS_ARCH := arm
-TARGET_DENSITY := xhdpi
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_MINIMAL_APPS := true
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := titan
 PRODUCT_NAME := aosp_titan
